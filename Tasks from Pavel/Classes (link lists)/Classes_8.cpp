bd20ed45044a5db12cca8a3d05470fc07a3d3cd5
SoftMike <hostagedream@gmail.com>
2019-03-12 11:24:18 +0200

    Last task updated by the right way without mistakes.

--- Tasks from Pavel/Classes (link lists)/Classes_8.cpp
+++ Tasks from Pavel/Classes (link lists)/Classes_8.cpp
@@ -1,12 +1,245 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <string>
-#include "list.h"
+
+template <class T> class list
+{
+	T *data;
+	list<T> *next;
+public:
+	list();
+	~list();
+	T &operator[](int i);
+	void clear_data();
+	void add();
+	void display();
+	void search();
+	void delete_data();
+	void insert();
+protected:
+	void clear();
+};
+
+template <class T> list<T>::list()
+{
+	data = NULL;
+	next = NULL;
+}
+template <class T> list<T>::~list()
+{
+	clear();
+}
+template <class T> T &list<T>::operator[](int i)
+{
+	int n = 0;
+	list<T> *temp = this->next;
+	while (n != i && temp->next->next)
+	{
+		temp = temp->next;
+		n++;
+	}
+	if (n == i)
+		return *temp->data;
+	else
+		std::cout << "Invalid data id!\n";
+}
+template <class T> void list<T>::clear_data()
+{
+	if (this->next->data)
+	{
+		list<T> *top, *temp;
+		top = this->next;
+		temp = this->next;
+		while (temp->next)
+		{
+			temp = temp->next;
+			top->next = NULL;
+			delete top->data;
+			top = temp;
+		}
+		if (this->data)
+		{
+			delete this->data;
+			this->data = NULL;
+			this->next = NULL;
+		}
+	}
+}
+template <class T> void list<T>::add()
+{
+	std::cout << "Enter DATA:\n\n";
+	T text;
+	std::cin >> text;
+	if (!this->data)
+	{
+		this->data = new T;
+		this->next = new list<T>;
+		this->next->data = new T;
+		*this->next->data = text;
+		this->next->next = new list<T>;
+	}
+	else
+	{
+		list<T> *temp = this;
+		while (temp->next)
+			temp = temp->next;
+		temp->data = new T;
+		*temp->data = text;
+		temp->next = new list<T>;
+	}
+}
+template <class T> void list<T>::display()
+{
+	if (this->data)
+	{
+		list<T> *temp = this->next;
+		while (temp && temp->data)
+		{
+			std::cout << *temp->data << std::endl;
+			if (temp->next)
+				temp = temp->next;
+			else
+				break;
+		}
+	}
+	else
+		std::cout << "Data is empty\n";
+}
+template <class T> void list<T>::search()
+{
+	T text;
+	list<T> *temp = this;
+	std::cout << "Enter searching DATA:\n\n";
+	std::cin >> text;
+	while (*temp->data != text)
+	{
+		if (!temp->next->next)
+			break;
+		temp = temp->next;
+	}
+	if (*temp->data == text)
+	{
+		std::cout << "Founded DATA:\n\n";
+		std::cout << *temp->data << std::endl;
+	}
+	else
+		std::cout << "DATA didn't find...\n";
+}
+template <class T> void list<T>::delete_data()
+{
+	T text;
+	list<T> *temp = this;
+	std::cout << "Enter searching DATA:\n\n";
+	std::cin >> text;
+	if (*temp->data == text)
+	{
+		while (temp->next->next)
+		{
+			temp->data = temp->next->data;
+			temp = temp->next;
+		}
+		delete temp->next->data;
+		temp->next->data = NULL;
+		temp->next->next = NULL;
+	}
+	else
+	{
+		while (*temp->next->data != text)
+		{
+			if (!temp->next->next->next)
+				break;
+			temp = temp->next;
+		}
+		if (*temp->next->data == text)
+		{
+			std::cout << "Deleted data:\n\n";
+			std::cout << *temp->next->data << std::endl;
+			list<T> *del = temp->next;
+			temp->next = temp->next->next;
+			delete del->data;
+			del->next = NULL;
+			del->data = NULL;
+		}
+		else
+			std::cout << "Data didn't find...\n";
+	}
+}
+template <class T> void list<T>::insert()
+{
+	T text;
+	std::cout << "Enter inserting DATA:\n\n";
+	std::cin >> text;
+	list<T> *temp = this;
+	int n = 0;
+	int k = 0;
+	if (temp->next)
+	{
+		while (temp->next->next)
+		{
+			temp = temp->next;
+			n++;
+		}
+	}
+	else
+	{
+		std::cout << "Data is empty...\n";
+		return;
+	}
+	temp->next->next = new list<T>;
+	temp->next->data = new T;
+	k = n / 2;
+	T swap = (*this)[k];
+	T swap2;
+	(*this)[k++] = text;
+	if (n == 1)
+	{
+		(*this)[k] = swap;
+	}
+	else if (n == 2)
+	{
+		swap2 = (*this)[k];
+		(*this)[k++] = swap;
+	}
+	else
+	{
+		do
+		{
+			swap2 = (*this)[k];
+			(*this)[k++] = swap;
+			swap = swap2;
+		} while (k != n);
+		(*this)[k] = swap;
+	}
+
+}
+template <class T> void list<T>::clear()
+{
+	if (this->data)
+	{
+		list<T> *top, *temp;
+		top = this->next;
+		temp = this->next;
+		while (temp->next)
+		{
+			temp = temp->next;
+			top->next = NULL;
+			delete top->data;
+			top = temp;
+		}
+		if (this->data)
+		{
+			delete this->data;
+			this->data = NULL;
+			this->next = NULL;
+		}
+	}
+}
 
 int main()
 {
 	int choise = 0;
-	list base;
+	// DATA choise:
+	list<int> base;
 	do
 	{
 		system("cls");
